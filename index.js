9917e330c9be33b016c9525ddcafd74cd8160194
Alex <alexanderjukes@gmail.com>
2019-08-24 20:42:28 +0100

    Implement single move best result

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-const BOARD_SIZE = 8;
+const BOARD_SIZE = 8
 
-const node = () => ({
-  movesSoFar: [],
-  estimatedTimeToFinish: null,
-  overallScore: 0,
+const node = (position) => ({
+  f: 0,
+  g: 0,
+  h: 0,
   parent: {},
+  position
 })
 
-const generateChessBoard = () => Array.from(Array(BOARD_SIZE), () => new Array(BOARD_SIZE).fill(node()))
+const generateChessBoard = () => {
+  const grid = Array.from(Array(BOARD_SIZE), () => new Array(BOARD_SIZE).fill([]))
+  return grid.map((row, rowIndex) => row.map((_, colIndex) => node([rowIndex, colIndex])))
+}
 
-const getNextValidKnightMoves = ([x, y], grid) => {
+
+const getNextValidKnightMoves = ([x, y], grid = []) => {
   const moveSet = [[-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2]]
-  const [ row ] = grid;
+  const [ row ] = grid
   const isSquareWithinGrid = ([x, y]) => (x >= 0 && x <= grid.length) && (y >= 0 && y <= row.length)
   return moveSet.map(([diffX, diffY]) => [x + diffX, y + diffY]).filter((isSquareWithinGrid))
 }
 
-const chessNotationToBoardArrayMap = {
-    x: {
-      a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    e: 4,
-    f: 5,
-    g: 6,
-    h: 7,
-    },
-    y: {
-      1: 7,
-      2: 6,
-      3: 5,
-      4: 4,
-      5: 3,
-      6: 2,
-      7: 1,
-      8: 0,
-    },
-}
+const findBestMoveSet = (start, end, grid) => {
+  // const [startX, startY] = start
+  const [endX, endY] = end
+  // const startNode = grid[startX][startY]
+  
+  const possibleOpeningMoves = getNextValidKnightMoves(start, grid)
+  const openList = [...possibleOpeningMoves]
+  // const closedList = []
+  
+  if (openList.filter(([x,y]) =>(x === endX && y === endY))) {
+    return end
+  }
 
-const getBoardIndexFromNotation = (notation) => {
-  const [x, y] = notation;
-  return [chessNotationToBoardArrayMap.x[x], chessNotationToBoardArrayMap.y[y] ]
+  while (openList.length) {
+    const fList = openList.map(({f}) => f)
+    const lowestFIndex = fList.indexOf(Math.min(...fList))
+    let currentNode = openList[lowestFIndex]
+    
+    if (currentNode.position[0] == endX && currentNode.position[1] === endY) {
+      let curr = currentNode
+      let result = []
+      while(curr.parent) {
+        result.push(curr),
+        curr = curr.parent
+      }
+      return result.reverse
+    }
+  }
+  return []
 }
 
 module.exports = {
   generateChessBoard,
   getNextValidKnightMoves,
+  findBestMoveSet,
 }
