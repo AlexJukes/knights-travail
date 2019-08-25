1798607b6d87aa078e81209953f053f3c9b1b65c
Alex <alexanderjukes@gmail.com>
2019-08-25 12:00:31 +0100

    Implement single move best result

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BOARD_SIZE = 8;
+const BOARD_SIZE = 8
 
 const node = (position) => ({
   f: 0,
@@ -9,59 +9,47 @@ const node = (position) => ({
 })
 
 const generateChessBoard = () => {
-  const grid = Array.from(Array(BOARD_SIZE), () => new Array(BOARD_SIZE).fill([]));
+  const grid = Array.from(Array(BOARD_SIZE), () => new Array(BOARD_SIZE).fill([]))
   return grid.map((row, rowIndex) => row.map((_, colIndex) => node([rowIndex, colIndex])))
 }
 
 
 const getNextValidKnightMoves = ([x, y], grid = []) => {
   const moveSet = [[-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2]]
-  const [ row ] = grid;
+  const [ row ] = grid
   const isSquareWithinGrid = ([x, y]) => (x >= 0 && x <= grid.length) && (y >= 0 && y <= row.length)
   return moveSet.map(([diffX, diffY]) => [x + diffX, y + diffY]).filter((isSquareWithinGrid))
 }
 
 const findBestMoveSet = (start, end, grid) => {
-  const [startX, startY] = start;
-  const [endX, endY] = end;
-  const startNode = grid[startX][startY];
-  console.log('start', start);
+  // const [startX, startY] = start
+  const [endX, endY] = end
+  // const startNode = grid[startX][startY]
   
-  const possibleOpeningMoves = getNextValidKnightMoves(start, grid);
-  console.log('possibleOpeningMoves', possibleOpeningMoves);
-  const openList = [...possibleOpeningMoves];
-  const closedList = [];
-
-  let result = openList.map(console.log).filter();
-  console.log('result', result);
-
-  if(openList.map(([x,y]) => (x === endX && y === endY)).filter()) {
-    return end;
+  const possibleOpeningMoves = getNextValidKnightMoves(start, grid)
+  const openList = [...possibleOpeningMoves]
+  // const closedList = []
+  
+  if (openList.filter(([x,y]) =>(x === endX && y === endY))) {
+    return end
   }
 
-  while(openList.length) {
+  while (openList.length) {
     const fList = openList.map(({f}) => f)
-    const lowestFIndex = fList.indexOf(Math.min(...fList));
-    let currentNode = openList[lowestFIndex];
-    console.log('currentNode', currentNode);
-    console.log('currentNode.position[0]', currentNode.position[0]);
-    console.log('currentNode.position[1]', currentNode.position[1]);
-    console.log('endY', endY);
-    console.log('endX', endX);
+    const lowestFIndex = fList.indexOf(Math.min(...fList))
+    let currentNode = openList[lowestFIndex]
     
     if (currentNode.position[0] == endX && currentNode.position[1] === endY) {
-      let curr = currentNode;
-      let result = [];
-      console.log('curr.parent', curr.parent);
-      console.log('result', result);
+      let curr = currentNode
+      let result = []
       while(curr.parent) {
         result.push(curr),
-        curr = curr.parent;
+        curr = curr.parent
       }
-      return result.reverse;
+      return result.reverse
     }
   }
-  return [];
+  return []
 }
 
 module.exports = {
--- test/index.test.js
+++ test/index.test.js
@@ -1,40 +1,41 @@
-require('mocha');
-const { expect} = require('chai');
-const { generateChessBoard, getNextValidKnightMoves, findBestMoveSet } = require('..');
+/* eslint-disable no-undef */
+require('mocha')
+const { expect} = require('chai')
+const { generateChessBoard, getNextValidKnightMoves, findBestMoveSet } = require('..')
 
-describe("Knight's Travails", () => {
+describe('Knight\'s Travails', () => {
   describe('generateChessBoard', () => {
-    it("will generate and 8 x 8 grid of Nodes", () => {
-      const chessBoard = generateChessBoard();
+    it('will generate and 8 x 8 grid of Nodes', () => {
+      const chessBoard = generateChessBoard()
       expect(chessBoard.length).to.equal(8)
       chessBoard.map((row, rowIndex) => {
-        expect(row.length).to.equal(8);
+        expect(row.length).to.equal(8)
         row.map((node, colIndex) => {
-          expect(Object.keys(node)).to.include('f', 'g', 'h', 'parent', 'position');
+          expect(Object.keys(node)).to.include('f', 'g', 'h', 'parent', 'position')
           expect(node.position).to.deep.equal([rowIndex, colIndex])
         })
       })
     })
   }),
   describe('generateNextValidKnightMoves', () => {
-    it("given a current position, will generate and array of possible valid positions", () => {
-      const chessBoard = generateChessBoard();
-      const nextMoves = getNextValidKnightMoves([3,4], chessBoard);
-      console.log('nextMoves', nextMoves);
+    it('given a current position, will generate and array of possible valid positions', () => {
+      const chessBoard = generateChessBoard()
+      const nextMoves = getNextValidKnightMoves([3,4], chessBoard)
+      console.log('nextMoves', nextMoves)
       
-      expect(nextMoves).to.deep.equal([[2,6], [1,5], [1, 3], [2, 2], [4, 2], [5,3], [5,5], [4,6]]);
+      expect(nextMoves).to.deep.equal([[2,6], [1,5], [1, 3], [2, 2], [4, 2], [5,3], [5,5], [4,6]])
     }),
-    it("given a current position and a grid, will find all possible moves within the confines of the grid", () => {
-      const chessBoard = generateChessBoard();
-      const nextMoves = getNextValidKnightMoves([0,0], chessBoard);
-      expect(nextMoves).to.deep.equal([[2,1], [1,2]]);
+    it('given a current position and a grid, will find all possible moves within the confines of the grid', () => {
+      const chessBoard = generateChessBoard()
+      const nextMoves = getNextValidKnightMoves([0,0], chessBoard)
+      expect(nextMoves).to.deep.equal([[2,1], [1,2]])
     })
   })
   describe('findBestMoveSet', () => {
-    it("given a current position, and an end position one move away, will return the correct final move", () => {
-      const chessBoard = generateChessBoard();
+    it('given a current position, and an end position one move away, will return the correct final move', () => {
+      const chessBoard = generateChessBoard()
       const bestMoveSet = findBestMoveSet([3,4], [2,6], chessBoard)
-      expect(bestMoveSet).to.deep.equal([[2,6]]);
+      expect(bestMoveSet).to.deep.equal([2,6])
     })
   })
 })
